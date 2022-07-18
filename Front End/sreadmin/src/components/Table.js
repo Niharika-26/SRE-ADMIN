3d3ee0e2ec544c79f4d3dced3c93c36a5b014619
dikshantdv <vyasdikshant1210@gmail.com>
2022-07-18 15:05:52 +0530

    globalization in table added

--- Front End/sreadmin/src/components/Table.js
+++ Front End/sreadmin/src/components/Table.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-
+import { useTranslation } from "react-i18next";
 import { Table, Button } from "antd";
 import { HistoryOutlined } from "@ant-design/icons";
 
 function TableComponent() {
+  const { t } = useTranslation();
   const [page, setPage] = useState(1);
   const data = [
     {
@@ -118,53 +119,61 @@ function TableComponent() {
 
   const columns = [
     {
-      title: "Sr. No.",
+      title: t("c1"),
       dataIndex: "key",
       key: "key",
       render: (value, item, index) => (page - 1) * 10 + index + 1,
     },
     {
-      title: "Task name ",
+      title: t("c2"),
       dataIndex: "Task_name",
       key: "Task_name",
+      render: (value) => t(value),
     },
     {
-      title: "Environment name ",
+      title: t("c3"),
       dataIndex: "Environment_name",
       key: "Environment_name",
+      render: (value) => t(value),
     },
     {
-      title: "Request data ",
+      title: t("c4"),
       dataIndex: "Request_data",
       key: "Request_data",
+      render: (value) => t(value),
     },
     {
-      title: "Response data ",
+      title: t("c5"),
       dataIndex: "Response_data",
       key: "Response_data",
+      render: (value) => t(value),
     },
     {
-      title: "Due date ",
+      title: t("c6"),
       dataIndex: "Due_date",
       key: "Due_date",
+      render: (value) => t(value),
     },
     {
-      title: "Created date ",
+      title: t("c7"),
       dataIndex: "Created_date",
       key: "Created_date",
+      render: (value) => t(value),
     },
     {
-      title: "Start date ",
+      title: t("c8"),
       dataIndex: "Start_date",
       key: "Start_date",
+      render: (value) => t(value),
     },
     {
-      title: "Status ",
+      title: t("c9"),
       dataIndex: "Task_name",
       key: "Task_name",
+      render: (value) => t(value),
     },
     {
-      title: "Reschedule",
+      title: t("c10"),
       data: "",
       key: "key",
       render: (text, record) => (
