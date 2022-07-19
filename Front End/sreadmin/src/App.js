0aa1fe369a7f2536cec29b73352eeb056de0ea2b
arunabh-jain <btech19eskcs046@skit.ac.in>
2022-07-19 21:40:28 +0530

    front end styling

--- Front End/sreadmin/src/App.js
+++ Front End/sreadmin/src/App.js
@@ -16,30 +16,30 @@ import ModalComponent from "./components/Modal";
 
 function App() {
   const { t } = useTranslation();
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  // const [data, setData] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    i18n.changeLanguage(navigator.language);
-    if (localStorage.getItem("i18nextLng")?.length > 2) {
-      i18n.changeLanguage("en");
-    }
-  }, []);
-  const asyncFetch = () => {
-    setIsLoading(true)
-      fetch('http://localhost:8000/data')
-      .then((response) => response.json())
-      .then((json) => {
-          setData(json["tasks"]);
-          setIsLoading(false)
-      })
-      .catch((error) => {
-          console.log('fetch data failed', error);
-      });
-    };
-    useEffect(() => {
-      asyncFetch();
-  }, []);
+  // useEffect(() => {
+  //   i18n.changeLanguage(navigator.language);
+  //   if (localStorage.getItem("i18nextLng")?.length > 2) {
+  //     i18n.changeLanguage("en");
+  //   }
+  // }, []);
+  // const asyncFetch = () => {
+  //   setIsLoading(true)
+  //     fetch('http://localhost:8000/data')
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //         setData(json["tasks"]);
+  //         setIsLoading(false)
+  //     })
+  //     .catch((error) => {
+  //         console.log('fetch data failed', error);
+  //     });
+  //   };
+  //   useEffect(() => {
+  //     asyncFetch();
+  // }, []);
 
   return (
     <Fragment>
@@ -47,21 +47,17 @@ function App() {
         <img
           style={{
             display: "inline-block",
-            marginLeft: 200,
-            marginRight: 120,
-            marginTop: 10,
-            marginBottom: 25,
-            left: 200,
-            width: 70,
+            marginLeft: 10,
+            marginTop: 7.5,
+            width: 50,
           }}
           src="https://www.decurtis.com/wp-content/uploads/2019/11/logo-decurtis-corporation.png"
           alt="Logo"
         />
 
         <h1 className="project-title">{t("Title")}</h1>
-      </div>
 
-      <div className="top">
+        
         <div className="searchbar">
           <SearchFilter />
         </div>
@@ -72,7 +68,7 @@ function App() {
 
       <div className="tablediv">
         <div className="table">
-          <TableComponent data={data} isLoading={isLoading}/>
+          <TableComponent/> {/*data={data} isLoading={isLoading}*/}
         </div>
       </div>
     </Fragment>
