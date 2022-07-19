f9ed827ea6b4e20d9b060e5a9f6264b21f0fef4a
Mohini Garg <mohinigarg2001@gmail.com>
2022-07-19 12:43:00 +0530

    table status added

--- Front End/sreadmin/src/components/Table.js
+++ Front End/sreadmin/src/components/Table.js
@@ -1,119 +1,122 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Table, Button } from "antd";
+import { Table, Button, Tag } from "antd";
 import { HistoryOutlined } from "@ant-design/icons";
+
 
 function TableComponent() {
   const { t } = useTranslation();
   const [page, setPage] = useState(1);
+ 
   const data = [
     {
       Task_name: "name1",
       Environment_name: "env",
-      Request_data: "data",
+      
       Response_data: "data22",
-      Due_date: 23,
-      Created_date: 24,
-      Start_date: 1,
+      Due_at: '2/07/22',
+      Modified_at: '1/07/22',
+      Status: "Pending"
+     
     },
     {
-      Task_name: "naxszcxz2",
+      Task_name: "ship",
       Environment_name: "env",
-      Request_data: "data",
+      
       Response_data: "data22",
-      Due_date: 23,
-      Created_date: 24,
-      Start_date: 1,
+      Due_at: '23/07/22',
+      Modified_at: '10/07/22',
+      Status: "Failed"
     },
     {
       Task_name: "name3",
       Environment_name: "env",
-      Request_data: "data",
+      
       Response_data: "data22",
-      Due_date: 23,
-      Created_date: 24,
-      Start_date: 1,
+      Due_at: '18/07/22',
+      Modified_at: '3/07/22',
+      Status: "Failed"
     },
     {
       Task_name: "name4",
       Environment_name: "env",
-      Request_data: "data",
+      
       Response_data: "data22",
-      Due_date: 23,
-      Created_date: 24,
-      Start_date: 1,
+      Due_at: '15/07/22',
+      Modified_at: '10/07/22',
+      Status: "In-Progress"
     },
     {
       Task_name: "name5",
       Environment_name: "env",
-      Request_data: "data",
+      
       Response_data: "data22",
-      Due_date: 23,
-      Created_date: 24,
-      Start_date: 1,
+      Due_at: '24/08/22',
+      Modified_at: '30/07/22',
+      Status: "Pending"
     },
     {
       Task_name: "name6",
       Environment_name: "env",
-      Request_data: "data",
+      
       Response_data: "data22",
-      Due_date: 23,
-      Created_date: 24,
-      Start_date: 1,
+      Due_at: '18/07/22',
+      Modified_at: '13/07/22',
+      Status: "In-Progress"
     },
     {
       Task_name: "name7",
       Environment_name: "env",
-      Request_data: "data",
+     
       Response_data: "data22",
-      Due_date: 23,
-      Created_date: 24,
-      Start_date: 1,
+      Due_at: '9/07/22',
+      Modified_at: '7/07/22',
+      Status: "Pending"
     },
     {
       Task_name: "name7",
       Environment_name: "env",
-      Request_data: "data",
+      
       Response_data: "data22",
-      Due_date: 23,
-      Created_date: 24,
-      Start_date: 1,
+      Due_at: '18/07/22',
+      Modified_at: '13/07/22',
+      Status: "Pending"
     },
     {
       Task_name: "name7",
       Environment_name: "env",
-      Request_data: "data",
+      
       Response_data: "data22",
-      Due_date: 23,
-      Created_date: 24,
-      Start_date: 1,
+      Due_at: '9/07/22',
+      Modified_at: '7/07/22',
+      Status: "Successful"
     },
     {
       Task_name: "name7",
       Environment_name: "env",
-      Request_data: "data",
+      
       Response_data: "data22",
-      Due_date: 23,
-      Created_date: 24,
-      Start_date: 1,
+      Due_at: '18/07/22',
+      Modified_at: '13/07/22',
+      Status: "Successful"
     },
     {
       Task_name: "name11",
       Environment_name: "env",
-      Request_data: "data",
+      
       Response_data: "data22",
-      Due_date: 23,
-      Created_date: 24,
-      Start_date: 1,
+      Due_at: '9/07/22',
+      Modified_at: '7/07/22',
+      Status: "Pending"
     },
     {
       Task_name: "name12",
       Environment_name: "env",
-      Request_data: "data",
+      
       Response_data: "data22",
-      Due_date: 23,
-      Created_date: 24,
-      Start_date: 1,
+      Due_at: '18/07/22',
+      Modified_at: '13/07/22',
+      Status: "Pending"
     },
   ];
 
@@ -136,44 +139,54 @@ function TableComponent() {
       key: "Environment_name",
       render: (value) => t(value),
     },
+    
     {
       title: t("c4"),
-      dataIndex: "Request_data",
-      key: "Request_data",
-      render: (value) => t(value),
-    },
-    {
-      title: t("c5"),
       dataIndex: "Response_data",
       key: "Response_data",
       render: (value) => t(value),
     },
     {
+      title: t("c5"),
+      dataIndex: "Due_at",
+      key: "Due_at",
+      render: (value) => t(value),
+    },
+    
+    {
       title: t("c6"),
-      dataIndex: "Due_date",
-      key: "Due_date",
+      dataIndex: "Modified_at",
+      key: "Modified_at",
       render: (value) => t(value),
     },
     {
       title: t("c7"),
-      dataIndex: "Created_date",
-      key: "Created_date",
-      render: (value) => t(value),
+      dataIndex: "Status",
+      key: "Status",
+      // render: (value) => t(value),
+      render: (value) => {
+        let color = "blue";
+
+        if (value === 'Pending') {
+          color = 'blue';
+        }
+        else if(value === 'In-Progress') {
+          color = 'yellow'
+        }
+        else if(value === 'Failed') {
+          color = 'red'
+        }
+        else {color = 'green'}
+
+        return (
+          <Tag style={{"width":100, "text-align":"center"}} color={color} key={value}>
+            {value.toUpperCase()}
+          </Tag>
+        );
+  },
     },
     {
       title: t("c8"),
-      dataIndex: "Start_date",
-      key: "Start_date",
-      render: (value) => t(value),
-    },
-    {
-      title: t("c9"),
-      dataIndex: "Task_name",
-      key: "Task_name",
-      render: (value) => t(value),
-    },
-    {
-      title: t("c10"),
       data: "",
       key: "key",
       render: (text, record) => (
@@ -194,6 +207,7 @@ function TableComponent() {
           setPage(current);
         },
       }}
+      
     ></Table>
   );
 }
