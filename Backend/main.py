81314ee29859e3f5d3b498423444d08ec4da28eb
dikshantdv <vyasdikshant1210@gmail.com>
2022-08-02 10:43:52 +0530

    local variables

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 from router import tasks_get,tasks_post
 
@@ -6,7 +7,7 @@ from router import tasks_get,tasks_post
 app = FastAPI()
 
 # Add CORS middleware
-origins = ['http://localhost:3000','http://127.0.0.1:3000']
+origins = ['http://localhost:13000','http://127.0.0.1:13000']
 app.add_middleware(CORSMiddleware,
 allow_origins = origins,
 allow_credentials = True,
@@ -17,3 +18,6 @@ allow_headers = ['*']
 #  Add the routers
 app.include_router(tasks_get.router)
 app.include_router(tasks_post.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=18000)
